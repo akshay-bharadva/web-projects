fa788352f6eadc2fb1738611a97734d2beeb978b
Akshay Bharadva <52954931+AkshayBharadva@users.noreply.github.com>
2021-08-19 10:55:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 const quote = document.getElementById('quote');
 const quoteAuthor = document.getElementById('quoteAuthor');
 const quoteBtn = document.getElementById('quoteBtn');
-
-quoteBtn.addEventListener('click', generateQuote);
+const copyEl = document.getElementById('copy');
+const hiddenForCopy = document.getElementById('forCopy');
 
 generateQuote();
+quoteBtn.addEventListener('click', generateQuote);
+copyEl.addEventListener('click', copyQuoteToClipboard);
 
 function generateQuote() {
 	// ! From The Official Site
@@ -14,5 +16,15 @@ function generateQuote() {
 		.then((data) => {
 			quote.innerHTML = data.content;
 			quoteAuthor.innerHTML = `—${data.author}`;
+			hiddenForCopy.value = `"${data.content}" —${data.author}`;
 		});
+}
+function copyQuoteToClipboard() {
+	const copyQuote = hiddenForCopy;
+	copyQuote.select();
+	navigator.clipboard.writeText(copyQuote.value);
+	copyEl.innerHTML = `<i class="fas fa-copy"></i> Copied`;
+	setTimeout(() => {
+		copyEl.innerHTML = `<i class="far fa-copy"></i> Copy`;
+	}, 500);
 }
